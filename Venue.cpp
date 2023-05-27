0847e6918d11c6a61dbc0d649de4c5da73c10f29
CKoleva <kolevasimona04@gmail.com>
2023-05-27 14:35:27 +0300

    remove duplicated files

--- Venue.cpp
+++
@@ -1,8 +0,0 @@
-#include "Venue.h"
-
-Venue::Venue(const string& name)
-    : name(name) {}
-
-const string& Venue::getName() const {
-    return name;
-}
--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include "Engine.h"
-
-int main() 
-{    
-    Engine::getInstance().start();
-}
